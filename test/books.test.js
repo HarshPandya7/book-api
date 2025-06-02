a4ecbbbcf2fc9ef34dae5224b3ab9709519428a0
Harsh Pandya <83078964+HarshPandya7@users.noreply.github.com>
2025-06-02 19:11:32 +1000

    books.test.js

--- test/books.test.js
+++ test/books.test.js
@@ -1,18 +1,19 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../src/app');
+const app = require('../app'); // ✅ Uses exported app
 
 chai.use(chaiHttp);
-const expect = chai.expect;
+const { expect } = chai;
 
 describe('Books API', () => {
-    it('should GET all books', (done) => {
-        chai.request('http://localhost:3000')
-            .get('/api/books')
-            .end((err, res) => {
-                expect(res).to.have.status(200);
-                expect(res.body).to.be.an('array');
-                done();
-            });
-    });
+  it('should GET all books', (done) => {
+    chai.request(app)
+      .get('/api/books')
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('array'); // or object depending on your response
+        done();
+      });
+  });
 });
